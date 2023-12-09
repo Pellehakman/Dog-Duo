3769ae5812229881834c422754c4e59578c920cb
Pellehakman <83712876+Pellehakman@users.noreply.github.com>
2023-12-09 01:41:52 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faHouse, faUser, faComments, faSearch, faCalendar } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {
+  faHome, // Changed from faCalendar to faHome
+  faUser,
+  faMessage,
+  faSearch,
+  faCalendar,
+} from "@fortawesome/free-solid-svg-icons";
 
-import DashboardScreen from './screens/DashboardScreen';
-import UserScreen from './screens/UserScreen';
-import ChatScreen from './screens/ChatScreen';
-import ExploreScreen from './screens/ExploreScreen';
-import CalenderScreen from './screens/CalenderScreen';
+import DashboardScreen from "./screens/DashboardScreen";
+import UserScreen from "./screens/UserScreen";
+import ChatScreen from "./screens/ChatScreen";
+import ExploreScreen from "./screens/ExploreScreen";
+import CalenderScreen from "./screens/CalenderScreen";
 
 const Tab = createBottomTabNavigator();
 
@@ -16,55 +22,43 @@ const App = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ color, size, focused }) => {
-            let icon;
-
-            if (route.name === 'dashboard') {
-              icon = faHouse;
-            } else if (route.name === 'user') {
-              icon = faUser;
-            } else if (route.name === 'chat') {
-              icon = faComments;
-            } else if (route.name === 'explore') {
-              icon = faSearch;
-            } else if (route.name === 'calender') {
-              icon = faCalendar;
-            }
-
-            // Use the focused property to determine if the tab is active
-            const iconColor = focused ? 'white' : 'black';
-
-            return (
-              <FontAwesomeIcon
-                icon={icon}
-                size={20}
-                color={iconColor}
-               
-              />
-            );
-          },
-          tabBarLabel: '', // Set tabBarLabel to an empty string to hide the label
-          tabBarStyle: {
-            height: 80,
-            paddingHorizontal: 8,
-            borderRadius: 4,
-            paddingTop: 8,
-            paddingBottom: 8,
-            backgroundColor: 'white',
-            borderTopWidth: 0,
-          },
-        })}
-        tabBarOptions={{
-          activeBackgroundColor: '#EB0A5D',
-          activeTintColor: 'black', // Set the color when the tab is active
+        initialRouteName="Feed"
+        screenOptions={{
+          tabBarActiveTintColor: "#e91e63",
+          headerShown: false,
         }}
       >
-        <Tab.Screen name="dashboard" component={DashboardScreen} />
-        <Tab.Screen name="calender" component={CalenderScreen} />
-        <Tab.Screen name="explore" component={ExploreScreen} />
-        <Tab.Screen name="chat" component={ChatScreen} />
-        <Tab.Screen name="user" component={UserScreen} />
+        <Tab.Screen
+          name="Feed"
+          component={DashboardScreen}
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesomeIcon icon={faHome} color={color} size={size} /> // Changed from faCalendar to faHome
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Notifications"
+          component={UserScreen}
+          options={{
+            tabBarLabel: "Updates",
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesomeIcon icon={faMessage} color={color} size={size} /> // Use the correct icon
+            ),
+            tabBarBadge: 1,
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={ChatScreen}
+          options={{
+            tabBarLabel: "Profile",
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesomeIcon icon={faUser} color={color} size={size} /> // Use the correct icon
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );

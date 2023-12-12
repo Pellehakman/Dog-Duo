3f16091f7acfae102e72644bf6dd0a6e65476245
Pellehakman <83712876+Pellehakman@users.noreply.github.com>
2023-12-12 21:53:56 +0100

    1

--- components/dashboard/Dashboard.jsx
+++ components/dashboard/Dashboard.jsx
@@ -9,46 +9,16 @@ const Dashboard = () => {
   const items = Array.from({ length: 20 }, (_, index) => `Item ${index + 1}`);
 
   return (
-<<<<<<< HEAD
     <ScrollView contentContainerStyle={styles.dashboard}>
       {items.map((item, index) => (
         <View key={index} style={styles.item}>
           <Text>{item}</Text>
         </View>
       ))}
-=======
-    <ScrollView
-      contentContainerStyle={localStyles.scrollViewContainer}
-      showsVerticalScrollIndicator={false}
-    >
-      {/* <View style={localStyles.dashboardWrapper}>
-        <View style={localStyles.dashboardContainer}>
-          <View style={globalStyles.headingXLContainer}>
-            <Text style={globalStyles.H3}>Kommande aktiviteter</Text>
-          </View>
-          <ScrollView
-            contentContainerStyle={localStyles.horizontalScrollView}
-            horizontal
-            showsHorizontalScrollIndicator={false}
-          >
-            <DashboardItem />
-            <DashboardItem />
-          </ScrollView>
-        </View>
-        
-      </View> */}
-      <View>
-        <Text>hej</Text>
-      </View>
-      <View>
-        <Text>hej</Text>
-      </View>
->>>>>>> 2ad43e9579abebc683ac235f2d2c7657bb1918f9
     </ScrollView>
   );
 };
 
-<<<<<<< HEAD
 const styles = StyleSheet.create({
   dashboard: {
     flexGrow: 1,
@@ -57,21 +27,6 @@ const styles = StyleSheet.create({
     padding: 16,
     borderBottomWidth: 1,
     borderBottomColor: "#ccc",
-=======
-const localStyles = StyleSheet.create({
-  scrollViewContainer: {},
-  dashboardContainer: {
-    paddingBottom: height, // Adjust this value as needed
-  },
-  horizontalScrollView: {
-    paddingVertical: 12,
-  },
-  dashboardWrapper: {
-    paddingVertical: 12,
-    display: "flex",
-
-    flexDirection: "column",
->>>>>>> 2ad43e9579abebc683ac235f2d2c7657bb1918f9
   },
 });
 
